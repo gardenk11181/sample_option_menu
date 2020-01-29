5deada82c505eccfce01c3ce85d685bd942e34eb
gardenk11181 <55073876+gardenk11181@users.noreply.github.com>
2020-01-29 14:59:48 +0900

    Option Menu

--- app/src/main/java/com/gardenlab/sampleoptionmenu/MainActivity.java
+++ app/src/main/java/com/gardenlab/sampleoptionmenu/MainActivity.java
@@ -1,8 +1,12 @@
 package com.gardenlab.sampleoptionmenu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int curId = item.getItemId();
+        switch(curId) {
+            case R.id.menu_refresh:
+                Toast.makeText(this,"새로고침 메뉴 선택", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.menu_search:
+                Toast.makeText(this, "검색 메뉴 선택", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.menu_settings:
+                Toast.makeText(this, "설정 메뉴 선택", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
